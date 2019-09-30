8c0bdf10300af139160975c3b9be8e1032874cc3
jvera <jonatan.e.vera@gmail.com>
2019-09-30 10:52:22 -0300

    Se agrega modulo Personas y estilos

--- src/app/personas/personas.component.ts
+++ src/app/personas/personas.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-personas',
-    templateUrl: './personas.component.html'
+    templateUrl: './personas.component.html',
+    styleUrls: ['personas.component.css']
 })
 export class PersonasComponent{
 
